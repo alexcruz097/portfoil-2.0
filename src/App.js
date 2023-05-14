7e74d13900110a2d0bed4f5fd4e44ffcd57190ba
alexcruz097 <alexc017@yahoo.com>
2023-05-13 23:25:51 -0700

    started bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Home from "./sectionsLayout/home";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world.</h1>
+      <Home />
     </div>
   );
 }
