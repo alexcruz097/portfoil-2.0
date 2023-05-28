4fc4717d3494b261c74a322b4a08e3218d364640
alexcruz097 <alexc017@yahoo.com>
2023-05-28 12:02:42 -0700

    testing without home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./sectionsLayout/Home";
+
 import About from "./sectionsLayout/About";
 import Projects from "./sectionsLayout/Projects";
 import ButtonTop from "./sectionsLayout/ButtonTop";
@@ -10,7 +10,7 @@ import Footer from "./sectionsLayout/Footer";
 function App() {
   return (
     <div className="App">
-      <Home />
+
       <About />
       <Projects />
       <ButtonTop />
