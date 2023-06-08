387a56c63b40bbf7dae822137ec7f74ca34246c5
alexcruz097 <alexc017@yahoo.com>
2023-06-08 09:59:15 -0700

    added github logo for portfolio. top right corner.

--- src/sectionsLayout/GithubLogo.js
+++ src/sectionsLayout/GithubLogo.js
@@ -2,8 +2,8 @@ import React from "react";
 import gitHubIcon from "../image-icons/navGitHub.png";
 function GithubLogo() {
   return (
-    <div className="github-logo-container">
-      <img className="portfolio-gitHub-logo" src={gitHubIcon} />
+    <div>
+     <a href="https://github.com/alexcruz097/portfolio-2.0" target="_blank"> <img className="portfolio-gitHub-logo" src={gitHubIcon} /></a>
     </div>
   );
 }
