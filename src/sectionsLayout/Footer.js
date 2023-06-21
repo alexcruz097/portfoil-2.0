b1842de37ec19ce7a2e926684578de7dd84e10d1
alexcruz097 <alexc017@yahoo.com>
2023-06-21 13:51:23 -0700

    added animation to footer fonts

--- src/sectionsLayout/Footer.js
+++ src/sectionsLayout/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Slide } from "react-awesome-reveal";
 import { Container } from "react-bootstrap";
 
 function Footer() {
@@ -6,8 +7,10 @@ function Footer() {
   return (
     <footer className="footer py-3">
       <Container className="footer-inner">
+        <Slide direction="up" triggerOnce="true">
         <p className="h5">Alex Cruz&copy;{currentyear} </p>
         <p className="h5">Created with React Js</p>
+        </Slide>
       </Container>
     </footer>
   );
